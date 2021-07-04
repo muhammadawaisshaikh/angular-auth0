a88ae3e3d17a19a23ab5d474f4b3a57cec228d72
Muhammad Awais <awaisshaikh94@gmail.com>
2021-07-04 18:01:19 +0500

    app init with neccessary changes

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# angular-auth0
-Integrating Auth0 into your Angular applications
+# AngularAuth0
+
+## Configure Auth0 - Get Your Application Keys
+https://manage.auth0.com/
+
+## Configure Callback URLs, Configure Logout URLs, Configure Allowed Web Origins
+http://localhost:4200
+
+## Install the Auth0 Angular SDK
+> npm install @auth0/auth0-angular
+
+## Register and configure the authentication module - app.module.ts
+Import the AuthModule type from the @auth0/auth0-angular
+
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./components/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: 'homepage',
+    loadChildren: () => import('./components/homepage/homepage.module').then(m => m.HomepageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
